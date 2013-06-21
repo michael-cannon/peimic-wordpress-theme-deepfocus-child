eb25b5fba2db4646a35c9fe1e1b05316996d5bf5
Michael Cannon <michael@typo3vagabond.com>
2013-06-20 19:27:03 -0700

    Init import peimic-wordpress-theme-deepfocus-child

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ *  SCRIPT_SUMMARY
+ *
+ *  @author Michael Cannon <michael@typo3vagabond.com>
+ */
+
+include_once 'custom/custom_functions.php';
+?>
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php get_header(); ?>
+<?php
+if ( is_archive() ) {
+	remove_filter( 'the_content', 'rtsocial_counter' );
+	remove_filter( 'the_excerpt', 'rtsocial_counter' );
+}
+?>
+	<div id="content-full">
+		<div id="hr">
+			<div id="hr-center">
+				<div id="intro">
+					<div class="center-highlight">
+
+						<div class="container">
+
+							<?php get_template_part('includes/breadcrumbs'); ?>
+
+						</div> <!-- end .container -->
+					</div> <!-- end .center-highlight -->
+				</div>	<!-- end #intro -->
+			</div> <!-- end #hr-center -->
+		</div> <!-- end #hr -->
+
+		<div class="center-highlight">
+			<div class="container">
+
+				<?php $i = 1; ?>
+
+				<?php $blogcat = (int) get_catId(get_option('deepfocus_blog_cat')); ?>
+
+				<?php if ( (is_category() && in_subcat($blogcat,$cat)) || is_category( 255 ) || get_option('deepfocus_blog_style') == 'on' ) { ?>
+
+					<div id="content-area" class="clearfix">
+
+						<div id="left-area">
+							<?php get_template_part('includes/entry'); ?>
+						</div> <!-- end #left-area -->
+
+						<?php get_sidebar(); ?>
+
+					</div> <!-- end #content-area -->
+
+				<?php } else { ?>
+					<div id="gallery">
+						<div id="portfolio-items" class="clearfix">
+							<?php get_template_part('includes/gallery'); ?>
+						</div> <!-- end #portfolio-items -->
+					</div> <!-- end #gallery -->
+				<?php } ?>
+
+
+			</div> <!-- end .container -->
+
+			<?php get_footer(); ?>
